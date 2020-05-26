4c88cc86ce1430374fc258f7ba7a7b967b8ac8b9
kcode <kcode@kuaishou.com>
2020-05-26 10:24:20 +0800

    更新KcodeMain

--- src/main/java/com.kuaishou.kcode/KcodeMain.java
+++ src/main/java/com.kuaishou.kcode/KcodeMain.java
@@ -2,6 +2,7 @@ package com.kuaishou.kcode;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 
 /**
  * @author kcode
@@ -12,11 +13,14 @@ public class KcodeMain {
     public static void main(String[] args) throws Exception {
         // "demo.data" 是你从网盘上下载的测试数据，这里直接填你的本地绝对路径
         InputStream fileInputStream = new FileInputStream("demo.data");
-        KcodeQuestion question = new KcodeQuestion();
+        Class<?> clazz = Class.forName("com.kuaishou.kcode.KcodeQuestion");
+        Object instance = clazz.newInstance();
+        Method prepareMethod = clazz.getMethod("prepare", InputStream.class);
+        Method getResultMethod = clazz.getMethod("getResult", Long.class, String.class);
         // 准备数据
-        question.prepare(fileInputStream);
+        prepareMethod.invoke(instance, fileInputStream);
         // 验证正确性
-        String result = question.getResult(1L, "");
+        Object result = getResultMethod.invoke(instance, new Long(1L), "methodName");
         if ("result".equals(result)) {
             System.out.println("success");
         } else {
